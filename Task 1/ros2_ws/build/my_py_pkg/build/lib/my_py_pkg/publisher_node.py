148c99ad9933c19404620357e69174bf46ebf3a1
yousefh112 <yousef_hesham.yh112@icloud.com>
2025-01-10 12:53:05 +0200

    upadte

--- Task 1/ros2_ws/build/my_py_pkg/build/lib/my_py_pkg/publisher_node.py
+++ Task 1/ros2_ws/build/my_py_pkg/build/lib/my_py_pkg/publisher_node.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-import rclpy.logging
+
 
 class MinimalPublisher(Node):
 
@@ -10,7 +10,7 @@ class MinimalPublisher(Node):
         self.publisher_ = self.create_publisher(String, 'robot_status', 10)
         timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.timer_callback)
-
+        
         # Configure logging to exclude the timestamp
         rclpy.logging.set_logger_level(self.get_logger().name, rclpy.logging.LoggingSeverity.INFO)
         self.get_logger().info("Logging configured to exclude timestamp.")
@@ -21,6 +21,7 @@ class MinimalPublisher(Node):
         self.publisher_.publish(msg)
         self.get_logger().info(msg.data)
 
+
 def main(args=None):
     rclpy.init(args=args)
 
@@ -29,6 +30,7 @@ def main(args=None):
     rclpy.spin(minimal_publisher)
     minimal_publisher.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
---
+++ Task 1/ros2_ws/install/my_py_pkg/lib/python3.12/site-packages/my_py_pkg/subscriber_node.py
@@ -0,0 +1,37 @@
+import rclpy # type: ignore
+from rclpy.node import Node # type: ignore
+
+from std_msgs.msg import String # type: ignore
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('listener_node_2')
+        self.subscription = self.create_subscription(
+            String,
+            'robot_status',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard %s' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
